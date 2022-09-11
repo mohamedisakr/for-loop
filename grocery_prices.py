17c9dcef12bc2a3270f052042bf10e4e8e47aa18
mohamedisakr <mohamedisakr@gmail.com>
2022-09-11 07:58:27 +0200

    adding example 3

---
+++ grocery_prices.py
@@ -0,0 +1,8 @@
+groceryPrices = [200, 120, 10, 25, 110, 15, 75, 150]
+
+total_price = 0
+for index in range(len(groceryPrices)):
+    total_price = total_price + groceryPrices[index]
+    print('Current item:', groceryPrices[index])
+
+print('Total Price ', total_price)
