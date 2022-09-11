44d01f3d147c48beeb19d86b0590e86026613e0c
mohamedisakr <mohamedisakr@gmail.com>
2022-09-11 10:09:17 +0200

    review code

---
+++ email_quiz.py
@@ -0,0 +1,13 @@
+# Each student wants to create an email address.
+# The email address should be the student’s name @example.com
+# Print the email address for each student
+
+names = ['Benjamin', 'Allison', 'Philip', 'Dana', 'Emily']
+
+# for index in range(len(names)):
+#     print(names[index] + '@example.com')
+
+# or
+
+for name in names:
+    print(name + '@example.com')
--- grocery_items.py
+++ grocery_items.py
@@ -1,5 +1,10 @@
 groceryItems = ['Meat', 'Fish', 'Bread',
                 'Rice', 'Butter', 'Milk', 'Eggs', 'Cheese']
 
-for index in range(len(groceryItems)):
-    print('Current item:', groceryItems[index])
+# for index in range(len(groceryItems)):
+#     print('Current item:', groceryItems[index])
+
+# or
+
+for item in groceryItems:
+    print(item)
