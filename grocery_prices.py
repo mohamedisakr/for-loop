efb7e72464e431bd1626fafee8c2ff419e6dae4e
mohamedisakr <mohamedisakr@gmail.com>
2022-09-11 08:00:15 +0200

    adding example 4

--- grocery_prices.py
+++ grocery_prices.py
@@ -3,6 +3,6 @@ groceryPrices = [200, 120, 10, 25, 110, 15, 75, 150]
 total_price = 0
 for index in range(len(groceryPrices)):
     total_price = total_price + groceryPrices[index]
-    print('Current item:', groceryPrices[index])
+    print('Current price:', groceryPrices[index])
 
 print('Total Price ', total_price)
